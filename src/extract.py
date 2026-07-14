fcd6a114f35c194a64b3a1497b84b91bd213f35f
DieRow <ali.jahdkaran.1@gmail.com>
2026-07-15 01:50:43 +0330

    feat: add ccxt data downloader and raw BTC 1h historical data

---
+++ src/extract.py
@@ -0,0 +1,23 @@
+import ccxt
+import pandas as pd
+import time
+
+exchange = ccxt.binance({'enableRateLimit': True})
+
+start_time = exchange.parse8601('2023-01-01T00:00:00Z')
+all_candles = []
+print('Fetching data for BTC/USDT...')
+
+while True:
+    batch_data = exchange.fetch_ohlcv(symbol='BTC/USDT',timeframe='1h' , since=start_time, limit=1000)
+    if len(batch_data) == 0:
+        break
+    all_candles.extend(batch_data)
+    start_time = batch_data[-1][0] + 1
+    print(f'Downloaded {len(all_candles)} candles so far...')
+    time.sleep(1)
+
+
+df = pd.DataFrame(all_candles, columns=['Timestamp', 'Open', 'High', 'Low', 'Close', 'Volume'])
+df['Timestamp'] = pd.to_datetime(df['Timestamp'], unit='ms')
+df.to_csv('data\\raw\\btc_1h_1000candles.csv', index=False)
